67004ceb87be02d80433c6ef9e9988bef693bd98
Dongfang Li <15542377807@163.com>
2018-03-13 14:30:44 +0900

    Update chapter2.scm

--- chapter2.scm
+++ chapter2.scm
@@ -1,5 +1,86 @@
 #lang racket
 
+; exercise 2.1
+(define N 10)
+(define zero-bigit
+  (lambda ()
+    '()))
+(define zero-bigit?
+  (lambda (x)
+    (null? x)))
+(define successor-bigit
+  (lambda (n)
+    (cond
+      ((null? n) '(1))
+      ((> N (+ (car n) 1)) (cons (+ (car n) 1)))
+      (else (cons 0 (successor-bigit (cdr n)))))))
+(define predecessor-bigit
+  (lambda (n)
+    (cond
+      ((null? n) (epol:error ''))
+      ((and ((= 1 n) (cdr n)) (null? (cdr n))) '())
+      ((zero? (car n)) (cons (- N 1) (predecessor-bigit (cdr n))))
+      (else (cons (- (car n) 1) (cdr n))))))
+             
+; exercise 2.3
+(define one (lambda () '(one)))
+(define diff
+  (lambda (n1 n2)
+    (list 'diff n1 n2)))
+(define zero-diff?
+  (lambda ()
+    (diff (one) (one))))
+
+; exercise 2.4
+; (empty-stack)            = |[]|
+; (push v |[v1 v2 v3...]|) = |[v v1 v2 v3...]|
+; (pop |[v1 v2 v3...]|)    = |[v2 v3...]|
+; (top |[v1 v2 v3...]|)    = v1
+; (empty-stack? s)         = #t if s = |[]|, else #f
+;
+; constructors: empty-stack, push
+; observers: pop, top, empty-stack?
+    
+
+
+; note 2.2.2 data structure representation
+(define empty-env
+  (lambda () (list 'empty-env)))
+(define extend-env
+  (lambda (var val env)
+    (list 'extend-env var val env)))
+(define apply-env
+  (lambda (env search-var)
+    (cond
+      ((eqv? (car env) 'empty-env)
+       (report-no-binding-found search-var))
+      ((eqv? (car env) 'extend-env)
+       (let ((saved-var (cadr env))
+             (saved-val (caddr env))
+             (saved-env (cadddr env)))
+         (if (env? search-var saved-var)
+             saved-val
+             (apply-env saved-env search-var))))
+      (else
+       (report-invalid-env env)))))
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env "No binding for ~s" search-var)))
+(define report-invalid-env
+  (lambda (env)
+    (eopl:error 'apply-env "Bad environment: ~s" env)))
+       
+      
+
+
+
+
+
+
+
+
+
+
 ; exercise 2.5
 (define empty-env
   (lambda ()
@@ -63,7 +144,54 @@
 
 
 
-    
+; exercise 2.12
+
+
+
+; exercise 2.13
+
+
+
+; exercise 2.14
+
+
+
+
+
+;note 2.2.3 procedural representation
+(define empty-env
+  (lambda ()
+    (lambda (search-var)
+      (report-no-binding-found search-var))))
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (lambda (search-var)
+      (if (eqv? search-var saved-var)
+          saved-val
+          (apply-env saved-env search-var)))))
+(define apply-env
+  (lambda (env search-var)
+    (env search-var)))
+
+
+
+
+
+
+; note 2.3 interfaces for Recursive Data Types
+
+(define occurs-free?
+  (lambda (search-var exp)
+    (cond
+      ((var-exp? exp) (eqv? search-var (var-exp->var exp)))
+      ((lambda-exp? exp)
+       (and
+        (not (eqv? search-var (lambda-exp->bound-var exp)))
+        (occurs-free? search-var (lambda-exp->body exp)))
+       (else
+        (or
+         (occurs-free? search-var (app-exp->rator exp))
+         (occurs-free? search-var (app-exp->rand exp))))))))
    
         
         
