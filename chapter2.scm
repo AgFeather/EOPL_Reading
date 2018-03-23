e8b5e02b18c24ceee4e0d60ba219cd2cd78de2d8
Dongfang Li <15542377807@163.com>
2018-03-23 15:13:58 +0900

    Update chapter2.scm

--- chapter2.scm
+++ chapter2.scm
@@ -228,9 +228,79 @@
 
 ; exercise 2.13
 
+; empty-env: ()->Env
+(define empty-env
+  (lambda ()
+    (cons (lambda (search-var)
+            (error "no binding for ~s" search-var))
+           (lambda ()
+             #t))))
+
+;extend-env: var*schemeval*env -> env
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (cons (lambda (search-var)
+            (if (eqv? search-var saved-var)
+                saved-val
+                (apply-env saved-env search-var)))
+          (lambda ()
+            #f))))
+
+; apply-env: env * var -?schemeval
+(define apply-env
+  (lambda (env search-var)
+    ((car env) search-var)))
+
+(define empty-env?
+  (lambda (env)
+    ((cdr env))))
 
 
-; exercise 2.14
+(define e
+  (extend-env 'd 6
+     (extend-env 'y 8
+        (extend-env 'x 7
+           (extend-env 'y 14
+              (empty-env))))))
+
+
+
+; exercise 1.4
+(define empty-env
+  (lambda ()
+    (list (lambda (search-var)
+            (report-on-binding-found search-var))
+          (lambda (search-var)
+            #f)
+          (lambda ()
+            #t))))
+
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (list (lambda (search-var)
+            (if (eqv? search-var saved-var)
+                saved-var
+                (apply-env saved-env search-var)))
+          (lambda (search-var)
+            (if (eqv? search-var saved-var)
+                #t
+                (has-binding? saved-env search-var)))
+          (lambda ()
+            #f))))
+
+(define apply-env
+  (lambda (env search-var)
+    ((car env) search-var)))
+
+(define has-binding?
+  (lambda (env search-var)
+    ((cadr env) search-var)))
+
+(define empty-env?
+  (lambda (env)
+    ((caddr env))))
+
+
 
 
 
@@ -258,8 +328,14 @@
 
 
 
-; note 2.3 interfaces for Recursive Data Types
-    
+
+
+
+
+
+; note for 2.3 interfaces for recursive data type
+
+;occurs-free?: sym * LcExp -> Bool
 (define occurs-free?
   (lambda (search-var exp)
     (cond
@@ -267,10 +343,236 @@
       ((lambda-exp? exp)
        (and
         (not (eqv? search-var (lambda-exp->bound-var exp)))
-        (occurs-free? search-var (lambda-exp->body exp)))
-       (else
-        (or
-         (occurs-free? search-var (app-exp->rator exp))
-         (occurs-free? search-var (app-exp->rand exp))))))))
-      
+        (occurs-free? search-var (lambda-exp->body exp))))
+      (else
+       (or
+        (occurs-free? search-var (app-exp->rator exp))
+        (occurs-free? search-var (app-exp-rand exp)))))))
+
+
+
+;exercise 2.15
+(define var-exp
+  (lambda (var)
+    (cons 'var-exp var)))
+
+(define lambda-exp
+  (lambda (var lc-exp)
+    (list 'lambda-exp (list var) lc-exp)))
+
+(define app-exp
+  (lambda (lc-exp1 lc-exp2)
+    (list 'app-exp lc-exp1 lc-exp2)))
+
+;; var-exp? : Lc-exp -> Bool
+(define var-exp?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) 'var-exp))))
+
+;; lambda-exp? : Lc-exp -> Bool
+(define lambda-exp?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) 'lambda-exp))))
+
+;; app-exp? : Lc-exp -> Bool
+(define app-exp?
+  (lambda (x)
+    (and (list? x) (eq? (car x) 'app-exp))))
+
+;; var-exp->var : Lc-exp -> Var
+(define var-exp->var
+  (lambda (x)
+    (cdr x)))
+
+;; lambda-exp->bound-var : Lc-exp -> Var
+(define lambda-exp->bound-var
+  (lambda (x)
+    (caadr x)))
+
+;; lambda-exp->body : Lc-exp -> Lc-exp
+(define lambda-exp->body
+  (lambda (x)
+    (caddr x)))
+
+;; app-exp->rator : Lc-exp -> Lc-exp
+(define app-exp->rator
+  (lambda (x)
+    (cadr x)))
+
+;; app-exp->rand : Lc-exp -> Lc-exp
+(define app-exp->rand
+  (lambda (x)
+    (caddr x)))
+
+
+
+
+
+
+;exercise 2.16
+;; var-exp : Var -> Lc-exp
+(define var-exp
+  (lambda (var)
+    `(var-exp ,var)))
+
+;; lambda-exp : Var * Lc-exp -> Lc-exp
+(define lambda-exp
+  (lambda (var lc-exp)
+    `(lambda-exp ,var ,lc-exp)))
+
+;; app-exp : Lc-exp * Lc-exp -> Lc-exp
+(define app-exp
+  (lambda (lc-exp1 lc-exp2)
+    `(app-exp ,lc-exp1 ,lc-exp2)))
+
+;; var-exp? : Lc-exp -> Bool
+(define var-exp?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) 'var-exp))))
+
+;; lambda-exp? : Lc-exp -> Bool
+(define lambda-exp?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) 'lambda-exp))))
+
+;; app-exp? : Lc-exp -> Bool
+(define app-exp?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) 'app-exp))))
+
+;; var-exp->var : Lc-exp -> Var
+(define var-exp->var
+  (lambda (x)
+    (cadr x)))
+
+;; lambda-exp->bound-var : Lc-exp -> Var
+(define lambda-exp->bound-var
+  (lambda (x)
+    (cadr x)))
+
+;; lambda-exp->body : Lc-exp -> Lc-exp
+(define lambda-exp->body
+  (lambda (x)
+    (caddr x)))
+
+;; app-exp->rator : Lc-exp -> Lc-exp
+(define app-exp->rator
+  (lambda (x)
+    (cadr x)))
+
+;; app-exp->rand : Lc-exp -> Lc-exp
+(define app-exp->rand
+  (lambda (x)
+    (caddr x)))
+
+;; occurs-free? : Sym * Lcexp -> Bool
+(define occurs-free?
+  (lambda (search-var exp)
+    (cond
+     ((var-exp? exp) (eqv? search-var (var-exp->var exp)))
+     ((lambda-exp? exp)
+      (and
+       (not (eqv? search-var (lambda-exp->bound-var exp)))
+       (occurs-free? search-var (lambda-exp->body exp))))
+     (else
+      (or
+       (occurs-free? search-var (app-exp->rator exp))
+       (occurs-free? search-var (app-exp->rand exp)))))))
+
+
+
+
+;exercise 2.17
+
+
+
+
+
+; exercise 2.18
+(define (number->sequence node)
+	`(,node () ()))
+
+(define (current-element lst)
+	(car lst))
+
+(define (at-left-end? lst)
+	(null? (cadr lst)))
+
+(define (at-right-end? lst)
+	(null? (caddr lst)))
+
+(define (move-to-left lst)
+	(if (at-left-end? lst)
+		(report-at-the-end-of-left)
+		(list (caadr lst) (cdadr lst) (cons (car lst) (caddr lst)))))
+
+(define (move-to-right lst)
+	(if (at-right-end? lst)
+		(report-at-the-end-of-right)
+		(list (caaddr lst) (cons (car lst) (cadr lst)) (cdaddr lst))))
+
+(define (report-at-the-end-of-left)
+	(error 'move-to-left "Already at the end of the left."))
+
+(define (report-at-the-end-of-right)
+	(error 'move-to-right "Already at the end of the right."))
+
+(define (insert-to-left node lst)
+	(list (car lst) (cons node (cadr lst)) (caddr lst)))
+
+(define (insert-to-right node lst)
+	(list (car lst) (cadr lst) (cons node(caddr lst))))
+
+(equal?? 6 (current-element '(6 (5 4 3 2 1) (7 8 9))))
+(equal?? '(5 (4 3 2 1) (6 7 8 9)) (move-to-left '(6 (5 4 3 2 1) (7 8 9))))
+(equal?? '(7 (6 5 4 3 2 1) (8 9)) (move-to-right '(6 (5 4 3 2 1) (7 8 9))))
+(equal?? '(6 (13 5 4 3 2 1) (7 8 9)) (insert-to-left 13 '(6 (5 4 3 2 1) (7 8 9))))
+(equal?? '(6 (5 4 3 2 1) (13 7 8 9)) (insert-to-right 13 '(6 (5 4 3 2 1) (7 8 9))))
+
+
+
+
+; exercise 2.19
+
+(define (number->bintree node)
+	`(,node () ()))
+
+(define (current-element lst)
+	(car lst))
+
+(define (move-to-left-son bintree)
+	(cadr bintree))
+
+(define (move-to-right-son bintree)
+	(caddr bintree))
+
+(define at-leaf? null?)
+
+(define (as-left-branch left bintree)
+  (list (car bintree) left (move-to-right-son bintree)))
+
+(define (as-right-branch right bintree)
+  (list (car bintree) (move-to-left-son bintree) right))
+
+(define (insert-to-left node bintree)
+	(cond ((at-leaf? bintree) #f)
+		((pair? (move-to-left-son bintree))
+			(list (car bintree) (as-left-branch (move-to-left-son bintree) (number->bintree node)) (caddr bintree)))
+		(else (list (car bintree) (number->bintree node) (caddr bintree)))))
+
+(define (insert-to-right node bintree)
+	(cond ((at-leaf? bintree) #f)
+		((pair? (move-to-right-son bintree))
+			(list (car bintree) (cadr bintree) (as-right-branch (move-to-right-son bintree) (number->bintree node))))
+		(else (list (car bintree) (cadr bintree) (number->bintree node)))))
+
+(equal?? '(13 () ()) (number->bintree 13))
+(define t1 (insert-to-right 14
+				(insert-to-left 12
+					(number->bintree 13))))
+
+(equal?? '(12 () ()) (move-to-left-son t1))
+(equal?? 12 (current-element (move-to-left-son t1)))
+(equal?? #t (at-leaf? (move-to-right-son (move-to-left-son t1))))
+(equal?? (insert-to-left 15 t1) '(13 (15 (12 () ()) ()) (14 () ())))
         
